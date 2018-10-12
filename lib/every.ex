7637a8a26359c7f9a2f7bb3d1073c6155b0b0690
Sultan Iman <sultan@8fit.com>
2018-10-12 17:32:15 +0200

    docs: add initial module doc

--- lib/every.ex
+++ lib/every.ex
@@ -1,23 +1,33 @@
 defmodule Every do
   @moduledoc """
-  Documentation for Every.
+  Every gives you ability to use `Process.send_after/3`
+  with calculated intervals which can be rounded to every
+
+    1. Five minutes,
+    2. Fifteen minutes,
+    3. Hour,
+    4. Day (midnight)
   """
   @type initial_time() :: DateTime.t | NaiveDateTime.t
 
+  @spec round_to(initial_time()) :: integer()
   def five_minutes(initial_time) do
-    
+
   end
 
+  @spec round_to(initial_time()) :: integer()
   def fifteen_minutes(initial_time) do
-    
+
   end
 
+  @spec round_to(initial_time()) :: integer()
   def hour(initial_time) do
-    
+
   end
 
+  @spec round_to(initial_time()) :: integer()
   def day(initial_time) do
-    
+
   end
 
   @spec round_to(initial_time()) :: integer()
