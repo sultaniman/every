829f1472f84a185f325d8f36bc282daf03abdcf4
Sultan Iman <sultan@8fit.com>
2018-10-12 17:38:43 +0200

    docs: update moduledoc

--- lib/every.ex
+++ lib/every.ex
@@ -7,31 +7,36 @@ defmodule Every do
     2. Fifteen minutes,
     3. Hour,
     4. Day (midnight)
+
+  Every method accepts optional `initial_time` parameter
+  if it is provided then seconds til next call will be
+  calculated relative to give `initial_time`.
   """
   @type initial_time() :: DateTime.t | NaiveDateTime.t
+  @type round_value() :: DateTime.t | NaiveDateTime.t
 
   @spec round_to(initial_time()) :: integer()
-  def five_minutes(initial_time) do
+  def five_minutes(initial_time \\ nil) do
 
   end
 
   @spec round_to(initial_time()) :: integer()
-  def fifteen_minutes(initial_time) do
+  def fifteen_minutes(initial_time \\ nil) do
 
   end
 
   @spec round_to(initial_time()) :: integer()
-  def hour(initial_time) do
+  def hour(initial_time \\ nil) do
 
   end
 
   @spec round_to(initial_time()) :: integer()
-  def day(initial_time) do
+  def day(initial_time \\ nil) do
 
   end
 
-  @spec round_to(initial_time()) :: integer()
-  defp round_to(initial_time, round_value) do
+  @spec round_to(initial_time(), round_value()) :: integer()
+  defp round_to(initial_time \\ nil, round_value) do
     truncated = DateTime.truncate(initial_time, :seconds)
   end
 end
