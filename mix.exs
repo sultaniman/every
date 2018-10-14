b9cb23f996389cf1bb84bb377a4b310db627e72a
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-10-14 18:22:20 +0200

    chore: remove redundant comments

--- mix.exs
+++ mix.exs
@@ -33,9 +33,7 @@ defmodule Every.MixProject do
 
   defp package do
     [
-      # This option is only needed when you don't want to use the OTP application name
       name: "every",
-      # These are the default files included in the package
       files: ~w(lib .formatter.exs mix.exs README* LICENSE* src),
       licenses: ["Apache 2.0"],
       links: %{"GitHub" => "https://github.com/imanhodjaev/every"}
