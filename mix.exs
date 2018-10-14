34b0aea30b8e7826039cef369053d6fc0c22e238
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-10-14 18:21:09 +0200

    chore: prepare package

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Every.MixProject do
       version: "0.0.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package()
     ]
   end
 
@@ -26,6 +28,17 @@ defmodule Every.MixProject do
   defp deps do
     [
       {:timex, "~> 3.4"}
+    ]
+  end
+
+  defp package do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "every",
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE* src),
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/imanhodjaev/every"}
     ]
   end
 end
