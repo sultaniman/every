3c7299116b21657363b56a51643f8880ac07531c
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-10-14 18:30:52 +0200

    release: update version to 0.0.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Every.MixProject do
   def project do
     [
       app: :every,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
