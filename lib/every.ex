c9cb968a52051f1adb87365b3df067a900b6775d
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-10-14 17:47:34 +0200

    feat: implement and document all methods for Every

--- lib/every.ex
+++ lib/every.ex
@@ -14,16 +14,127 @@ defmodule Every do
   """
   use Timex
 
-  def five_minutes(relative_to \\ Timex.now()) do
-    next_due = get_next_interval(relative_to.minute, 5) - relative_to.minute
-    Timex.shift(relative_to, minutes: next_due)
-    |> get_diff(relative_to)
+  @doc """
+  Calculate how many seconds left
+  until next minute starts.
+
+  If `relative_to` `DateTime` is not provided
+  then current moment in time is used.
+
+  Returns: `integer` number of seconds
+
+  ## Example
+
+      iex> {:ok, now, _} = DateTime.from_iso8601("2018-10-14T16:48:12.000Z")
+      iex> Every.minute(now)
+      48
+  """
+  def minute(relative_to \\ Timex.now()) do
+    # Return seconds til next minute
+    60 - relative_to.second
   end
 
-  def fifteen_minutes(relative_to \\ Timex.now()) do
-    next_due = get_next_interval(relative_to.minute, 15) - relative_to.minute
-    Timex.shift(relative_to, minutes: next_due)
-    |> get_diff(relative_to)
+  @doc """
+  Calculate how many minutes left
+  until next interval when relative
+  `DateTime` is not provided and equals to `nil`.
+
+  Returns: `integer` number of seconds
+
+  ## Example
+
+      iex> {:ok, now, _} = DateTime.from_iso8601("2018-10-14T16:48:12.000Z")
+      iex> Every.minutes(5, now)  # 16:50 > 15:50:00 - 16:48:12
+      108
+  """
+  def minutes(interval, relative_to) when is_nil(relative_to) do
+    minutes(interval, Timex.now())
+  end
+
+  @doc """
+  Calculate how many minutes left
+  until next interval.
+
+  If `relative_to` `DateTime` is not provided
+  then current moment in time is used.
+
+  Returns: `integer` number of seconds
+
+  ## Example
+
+      iex> {:ok, now, _} = DateTime.from_iso8601("2018-10-14T16:48:12.000Z")
+      iex> Every.minutes(5, now)  # 16:50 > 15:50:00 - 16:48:12
+      108
+  """
+  def minutes(interval, relative_to) do
+    next_due = get_next_interval(relative_to.minute, interval) - relative_to.minute
+
+    get_diff(
+      Timex.shift(relative_to, minutes: next_due),
+      relative_to
+    )
+  end
+
+  @doc """
+  Calculate how many minutes left
+  until next hour starts.
+
+  If `relative_to` `DateTime` is not provided
+  then current moment in time is used.
+
+  Returns: `integer` number of seconds
+
+  ## Example
+
+      iex> {:ok, now, _} = DateTime.from_iso8601("2018-10-14T16:48:12.000Z")
+      iex> Every.hour(now)
+      768
+  """
+  def hour(relative_to \\ Timex.now()) do
+    minutes_left = 60 - relative_to.minute
+    seconds_left = 60 - relative_to.second
+    60 * minutes_left + seconds_left
+  end
+
+  @doc """
+  Calculate how many minutes left
+  until next hour starts.
+
+  If `relative_to` `DateTime` is not provided
+  then current moment in time is used.
+
+  Returns: `integer` number of seconds
+
+  ## Example
+
+      iex> {:ok, now, _} = DateTime.from_iso8601("2018-10-14T16:48:12.000Z")
+      iex> Every.hour(now)
+      768
+  """
+  def hours(interval, relative_to) when is_nil(relative_to) do
+    hours(interval, Timex.now())
+  end
+
+  @doc """
+  Calculate how many minutes left
+  until next hour starts.
+
+  If `relative_to` `DateTime` is not provided
+  then current moment in time is used.
+
+  Returns: `integer` number of seconds
+
+  ## Example
+
+      iex> {:ok, now, _} = DateTime.from_iso8601("2018-10-14T16:48:12.000Z")
+      iex> Every.hour(now)
+      768
+  """
+  def hours(interval, relative_to) do
+    next_due = get_next_interval(relative_to.hour, interval) - relative_to.hour
+    minutes_left = 60 - relative_to.minute
+    seconds_left = 60 - relative_to.second
+    3600 * next_due + 60 * minutes_left + seconds_left
   end
 
   defp get_diff(result, initial_time) do
