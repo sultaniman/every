15e6e4a013a5b2230f1cee7563a90c8b2bba1853
Sultan Iman <sultan.imanhodjaev@gmail.com>
2018-10-15 08:46:47 +0200

    chore: bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Every.MixProject do
   def project do
     [
       app: :every,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
