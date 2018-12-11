7c0e1b07fa6b0e51fd6af9685a690cb3aac9e780
Sultan Iman <354868+imanhodjaev@users.noreply.github.com>
2018-12-11 11:44:54 +0100

    chore: prepare new release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Every.MixProject do
   def project do
     [
       app: :every,
-      version: "0.0.6",
+      version: "0.0.7",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
