9311094b8a18847ef0e423667aaa9c5aa906a634
Sultan Iman <sultan.imanhodjaev@gmail.com>
2019-11-10 12:06:03 +0100

    Refactor mixfile

--- mix.exs
+++ mix.exs
@@ -1,15 +1,33 @@
 defmodule Every.MixProject do
   use Mix.Project
+  @vsn "0.0.9"
+
+  @deps [
+    {:timex, "~> 3.6"},
+    {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+    {:excoveralls, "~> 0.12", only: :test},
+    {:inch_ex, github: "rrrene/inch_ex", only: [:dev, :test]},
+    {:credo, "~> 1.0", only: [:dev, :test], runtime: false}
+  ]
+
+  @package [
+    name: "every",
+    files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
+    licenses: ["Apache 2.0"],
+    links: %{"GitHub" => "https://github.com/imanhodjaev/every"}
+  ]
+
+  @description "Calculate even intervals for `Process.send_after/3`"
 
   def project do
     [
       app: :every,
-      version: "0.0.8",
+      version: @vsn,
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps(),
-      description: description(),
-      package: package(),
+      deps: @deps,
+      description: @description,
+      package: @package,
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         coveralls: :test,
@@ -20,34 +38,9 @@ defmodule Every.MixProject do
     ]
   end
 
-  def description do
-    "Calculate even intervals for `Process.send_after/3`"
-  end
-
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger, :timex]
-    ]
-  end
-
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    [
-      {:timex, "~> 3.5"},
-      {:ex_doc, "~> 0.20", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.10", only: :test},
-      {:inch_ex, github: "rrrene/inch_ex", only: [:dev, :test]},
-      {:credo, "~> 1.0", only: [:dev, :test], runtime: false}
-    ]
-  end
-
-  defp package do
-    [
-      name: "every",
-      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
-      licenses: ["Apache 2.0"],
-      links: %{"GitHub" => "https://github.com/imanhodjaev/every"}
     ]
   end
 end
